daa672604f6b57d352cae613ec6ad91443d9a18c
Rob Looby <robertjlooby@gmail.com>
2014-03-21 19:17:19 -0500

    add UI selector and event binding

--- spec/side_chain/view.js
+++ spec/side_chain/view.js
@@ -1,8 +1,75 @@
-describe("SideChain.View", function() {
-  "use strict";
+describe('SideChain.View', function() {
+  'use strict';
 
-  it("is a Backbone.View", function() {
+  it('is a Backbone.View', function() {
     var view = new SideChain.View();
+
     expect(view instanceof Backbone.View).toBeTruthy();
   });
+
+  it('can access its UI elements', function() {
+    var myView = SideChain.View.extend({
+      ui: {
+        div1: '#div1',
+        div2: '#div2'
+      }
+    });
+    var view = new myView();
+
+    view.$el.html('<div id="div1"></div><div id="div2"></div>')
+
+    expect(view.$ui.div1()).toEqual(view.$('#div1'));
+    expect(view.$ui.div2()).toEqual(view.$('#div2'));
+  });
+
+  it('can bind UI selectors in events', function() {
+    var callback1 = jasmine.createSpy()
+    var callback2 = jasmine.createSpy()
+    var myView = SideChain.View.extend({
+      ui: {
+        div1: '#div1',
+        div2: '#div2'
+      },
+      events: {
+        'click {{ div1 }}' : callback1,
+        'click {{ div2 }}' : callback2
+      },
+      render: function() {
+        this.$el.html('<div id="div1"></div><div id="div2"></div>');
+        return this;
+      }
+    });
+    var view = new myView();
+
+    view.render();
+    view.$ui.div1().click()
+    view.$ui.div2().click()
+
+    expect(callback1).toHaveBeenCalled()
+    expect(callback2).toHaveBeenCalled()
+  });
+
+  it('can handle multiple UI selectors in events', function() {
+    var callback1 = jasmine.createSpy()
+    var myView = SideChain.View.extend({
+      ui: {
+        div1: '#div1',
+        div2: '#div2'
+      },
+      events: {
+        'click {{ div1 }} {{ div2 }}' : callback1
+      },
+      render: function() {
+        this.$el.html('<div id="div1"><div id="div2"></div></div>');
+        return this;
+      }
+    });
+    var view = new myView();
+
+    view.render();
+    view.$ui.div2().click()
+
+    expect(callback1).toHaveBeenCalled()
+  });
 });
+
--- src/side_chain/view.js
+++ src/side_chain/view.js
@@ -1,9 +1,37 @@
-namespace("SideChain");
+namespace('SideChain');
 
 (function(Backbone, $) {
-  "use strict";
+  'use strict';
 
-  SideChain.View = Backbone.View.extend({});
+  SideChain.View = Backbone.View.extend({
+    constructor: function(options) {
+      options || (options = {});
+      this.bindUISelectors();
+      this.bindUIEvents();
+      Backbone.View.apply(this, arguments);
+    },
+
+    bindUISelectors: function() {
+      this.$ui = {};
+      _.each(this.ui, (function(_this) {
+          return function(selector, name) {
+            _this.$ui[name] = function() {return _this.$(selector)}
+          };
+        })(this)
+      );
+    },
+
+    bindUIEvents: function() {
+      var tempEvents = this.events;
+      this.events = {};
+      _.each(tempEvents, (function(_this) {
+        return function(handler, eventTrigger) {
+          var interpolatedEventTrigger = _.template(eventTrigger, _this.ui, {interpolate: /\{\{(.+?)\}\}/g})
+          _this.events[interpolatedEventTrigger] = handler;
+        };
+      })(this)
+    );
+    }
+  });
 })(window.Backbone, window.jquery);
-
 
